e38124a32fbcfb2b7e2534698480601efa84f43c
David Sansome <me@davidsansome.com>
2019-08-21 19:22:04 +1000

    Create the dss.public.certs secret

--- config/apply-certs.sh
+++ config/apply-certs.sh
@@ -15,16 +15,17 @@ set -x
 DIR="$(pwd)"
 CLIENTS_CERTS_DIR="$DIR/generated/$NAMESPACE/client_certs_dir"
 NODE_CERTS_DIR="$DIR/generated/$NAMESPACE/node_certs_dir"
-CONTEXT="$(kubectl config current-context)"
 TEMPLATES_DIR="$DIR/templates"
 
 # Delete previous secrets in case they have changed.
-kubectl create namespace "$NAMESPACE" --context "$CONTEXT" || true
+kubectl create namespace "$NAMESPACE" || true
 
-kubectl delete secret cockroachdb.client.root --context "$CONTEXT" || true
-kubectl delete secret cockroachdb.client.root --namespace "$NAMESPACE" --context "$CONTEXT" || true
-kubectl delete secret cockroachdb.node --namespace "$NAMESPACE" --context "$CONTEXT" || true
+kubectl delete secret cockroachdb.client.root || true
+kubectl delete secret cockroachdb.client.root --namespace "$NAMESPACE" || true
+kubectl delete secret cockroachdb.node --namespace "$NAMESPACE" || true
+kubectl delete secret dss.public.certs --namespace "$NAMESPACE" || true
 
-kubectl create secret generic cockroachdb.client.root --from-file "$CLIENTS_CERTS_DIR" --context "$CONTEXT"
-kubectl create secret generic cockroachdb.client.root --namespace "$NAMESPACE" --from-file "$CLIENTS_CERTS_DIR" --context "$CONTEXT"
-kubectl create secret generic cockroachdb.node --namespace "$NAMESPACE" --from-file "$NODE_CERTS_DIR" --context "$CONTEXT"
+kubectl create secret generic cockroachdb.client.root --from-file "$CLIENTS_CERTS_DIR"
+kubectl create secret generic cockroachdb.client.root --namespace "$NAMESPACE" --from-file "$CLIENTS_CERTS_DIR"
+kubectl create secret generic cockroachdb.node --namespace "$NAMESPACE" --from-file "$NODE_CERTS_DIR"
+kubectl create secret generic dss.public.certs --namespace "$NAMESPACE" --from-file oauth.pem
