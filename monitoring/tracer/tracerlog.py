3af6b582d68871cccf2ed275ae42e3a028a6a827
Benjamin Pelletier <pelletierb@wing.com>
2021-01-11 14:59:25 -0800

    Add initial KML generation logic

--- monitoring/tracer/tracerlog.py
+++ monitoring/tracer/tracerlog.py
@@ -6,6 +6,8 @@ from typing import Dict
 
 import yaml
 
+from monitoring.monitorlib import infrastructure
+
 
 logging.basicConfig()
 _logger = logging.getLogger('tracer.logging')
@@ -13,10 +15,11 @@ _logger.setLevel(logging.DEBUG)
 
 
 class Logger(object):
-  def __init__(self, log_path: str):
+  def __init__(self, log_path: str, kml_session: infrastructure.KMLGenerationSession = None):
     self.log_path = log_path
     _logger.info('Log path: {}'.format(self.log_path))
     os.makedirs(self.log_path, exist_ok=True)
+    self.kml_session = kml_session
 
   def log_same(self, t0: datetime.datetime, t1: datetime.datetime, code: str) -> None:
     with open(os.path.join(self.log_path, '000000_nochange_queries.yaml'), 'a') as f:
@@ -36,5 +39,15 @@ class Logger(object):
     dump['object_type'] = type(content).__name__
     with open(fullname, 'w') as f:
       f.write(yaml.dump(dump, indent=2))
+
+    if self.kml_session:
+      with open(fullname, 'r') as f:
+        try:
+          kml_server_filename = os.path.join(self.kml_session.kml_folder, logname)
+          self.kml_session.post('/upload',
+                                data={'text': kml_server_filename},
+                                files={'file': f})
+        except IOError as e:
+          print('Error posting {} to KML server: {}'.format(kml_server_filename, e))
 
     return logname
